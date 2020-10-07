3295a3afe101d11a0d709c9bc87722c889e6db8d
Jesse Glick <jglick@cloudbees.com>
2020-10-07 13:51:02 -0400

    Compatibility with JEP-227

--- src/main/java/org/jenkinsci/plugins/reverse_proxy_auth/service/ProxyLDAPUserDetailsService.java
+++ src/main/java/org/jenkinsci/plugins/reverse_proxy_auth/service/ProxyLDAPUserDetailsService.java
@@ -17,9 +17,7 @@ import org.acegisecurity.userdetails.UsernameNotFoundException;
 import org.acegisecurity.userdetails.ldap.LdapUserDetails;
 import org.acegisecurity.userdetails.ldap.LdapUserDetailsImpl;
 import org.apache.commons.collections.map.LRUMap;
-import org.jenkinsci.plugins.reverse_proxy_auth.ReverseProxySecurityRealm;
 import org.springframework.dao.DataAccessException;
-import org.springframework.web.context.WebApplicationContext;
 
 public class ProxyLDAPUserDetailsService implements UserDetailsService {
 
@@ -36,11 +34,6 @@ public class ProxyLDAPUserDetailsService implements UserDetailsService {
 	 * reuse a session, this helps keeping the memory consumption low.
 	 */
 	private final LRUMap attributesCache = new LRUMap(32);
-
-	public ProxyLDAPUserDetailsService(ReverseProxySecurityRealm securityRealm, WebApplicationContext appContext) {
-		ldapSearch = securityRealm.extractBean(LdapUserSearch.class, appContext);
-		authoritiesPopulator = securityRealm.extractBean(LdapAuthoritiesPopulator.class, appContext);
-	}
 
 	public ProxyLDAPUserDetailsService(LdapUserSearch ldapSearch, LdapAuthoritiesPopulator authoritiesPopulator) {
 		this.ldapSearch = ldapSearch;
