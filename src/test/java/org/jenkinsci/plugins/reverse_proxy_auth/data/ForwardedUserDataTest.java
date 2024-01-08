83a292fe3ec3a3f602b645e7ab72360e28dda7a8
Alexander KRASINSKY <alexander.krasinsky@diehl-defence.com>
2024-01-08 15:52:25 +0100

    Fixed missing symbols

--- src/test/java/org/jenkinsci/plugins/reverse_proxy_auth/data/ForwardedUserDataTest.java
+++ src/test/java/org/jenkinsci/plugins/reverse_proxy_auth/data/ForwardedUserDataTest.java
@@ -2,34 +2,34 @@ import hudson.model.User;
 import hudson.tasks.Mailer;
 
 public class ForwardedUserDataTest {
-    private ForwardedUserData forwardedUserData
-    private User user
+    private ForwardedUserData forwardedUserData;
+    private User user;
 
     @Before
     public void setup() {
-        forwardedUserData = ForwardedUserData.new()
-        user = User.new()
+        forwardedUserData = ForwardedUserData.new();
+        user = User.new();
     }
 
     @Test
     public void basicForwardedUserData() {
-        forwardedUserData.setEmail("max.mustermann@example.com")
-        assertEquals(forwardedUserData.getEmail(), "max.mustermann@example.com")
+        forwardedUserData.setEmail("max.mustermann@example.com");
+        assertEquals(forwardedUserData.getEmail(), "max.mustermann@example.com");
 
-        forwardedUserData.setDisplayName("Max Mustermann")
-        assertEquals(forwardedUserData.getDisplayName(), "Max Mustermann")
+        forwardedUserData.setDisplayName("Max Mustermann");
+        assertEquals(forwardedUserData.getDisplayName(), "Max Mustermann");
     }
 
     @Test
     public void testUpdate() {
-        user.setFullName("John Doe")
-        forwardedUserData.setDisplayName("Max Mustermann")
-        forwardedUserData.setEmail("max.mustermann@example.com")
-        forwardedUserData.update(user)
+        user.setFullName("John Doe");
+        forwardedUserData.setDisplayName("Max Mustermann");
+        forwardedUserData.setEmail("max.mustermann@example.com");
+        forwardedUserData.update(user);
 
-        assertEquals(user.getFullName(), "Max Mustermann")
+        assertEquals(user.getFullName(), "Max Mustermann");
 
         Mailer.UserProperty emailProp = user.getProperty(Mailer.UserProperty.class);
-        assertEquals(emailProp.getConfiguredAddress(), "max.mustermann@example.com")
+        assertEquals(emailProp.getConfiguredAddress(), "max.mustermann@example.com");
     }
 }
